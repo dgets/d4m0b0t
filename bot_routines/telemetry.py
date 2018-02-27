059cea7a74369fa257e393b50989809fc00df657
Damon Getsman <damo.gets@gmail.com>
2018-02-27 08:42:52 -0600

    En route to #6 and #7.

--- bot_routines/telemetry.py
+++ bot_routines/telemetry.py
@@ -1,13 +1,14 @@
 import hlt
 
 from math import atan2, degrees, sqrt
-from . import myglobals
+from . import myglobals, analytics
 
 class EnemyData:
     def __init__(self, ship_id, x, y):
         if myglobals.DEBUGGING['enemy_data']:
             myglobals.log.debug("Initializing EnemyData for id: " + str(ship_id))
             
+        self.ship_entity = myglobals.game_map.get_me().get_ship(ship_id)
         self.id = ship_id
 
         #this is only run @ instantiation; too much shit is in here
@@ -18,6 +19,7 @@ class EnemyData:
 
         self.speed = None
         self.angle = None
+        self.target_weight = 0
         #self.turn_number = 0
         
     def update(self, x, y):
@@ -52,3 +54,45 @@ class EnemyData:
                 return True
             
         return False
+    
+    def determine_probable_target(self):
+        if speed == 0:
+            self.probable_target = None
+            self.target_weight = 0
+            return
+        
+        hit = False
+        for potential_destination in myglobals.game_map.all_planets():
+            if hlt.intersect_segment_circle(
+                self.ship_entity, potential_destination, {"x": self.x, "y": self.y, 
+                                                          "r": potential_destination.radius * myglobals.TARGET_INTERCEPT_FUDGE}):
+                hit = True
+                if self.probable_target == potential_destination and self.target_weight < myglobals.TARGET_WEIGHT_LIMIT:
+                    self.target_weight += 1
+                elif self.probable_target != potential_destination and self.target_weight > 0:
+                    self.target_weight -= 1
+                else: #if self.probable_target != potential_destination:
+                    self.probable_target = potential_destinion
+                    
+                #I do believe we'll weight on implementing target weight for a bit here
+                break
+
+        if not hit:
+            for potential_target in analytics.get_enemy_ships():
+                if hlt.intersect_segment_circle(
+                    self.ship_entity, potential_target, {"x": self.x, "y": self.y,
+                                                         "r": potential_target.radius * myglobals.TARGET_INTERCEPT_FUDGE}):
+                    hit = True
+                    if self.probable_target == potential_target and self.target_weight < myglobals.TARGET_WEIGHT_LIMIT:
+                        self.target_weight += 1
+                    elif self.probable_target != potential_target and self.target_weight > 0:
+                        self.target_weight -= 1
+                    else:
+                        self.probable_target = potential_target
+                        
+                    break
+                
+        if not hit:
+            self.probable_target = None
+            
+        
